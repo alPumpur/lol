12d7c8d2d5cfb182a0dcc1bdac6f4dc28d90f0b0
alPumpur <aleftinapumpur@gmail.com>
2023-11-17 21:31:47 +0700

    создание регистрации

--- bboard/main/models.py
+++ bboard/main/models.py
@@ -1,12 +1,35 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator, FileExtensionValidator
 
 
 class AdvUser(AbstractUser):
-    is_activated = models.BooleanField(default=True, db_index=True, verbose_name='Прошел активацию?')
+    login_validator = RegexValidator(
+        regex=r"^[a-zA-Z-]+$",
+        message="Введите допустимое слово. Должно содержать латиницу и дефисы."
+    )
 
-    send_messages = models.BooleanField(default=True, verbose_name='Оповещать при новых комментариях?')
+    fio_validator = RegexValidator(
+        regex=r"^[а-яА-Я-]+\s[а-яА-Я-]+\s[а-яА-Я-]+$",
+        message="Введите допустимое слово. Должно содержать кириллицу, пробелы и дефисы"
+    )
+
+    username = models.CharField(
+        verbose_name="Логин",
+        max_length=150,
+        unique=True,
+        blank=False,
+        help_text=(
+            "Разрешается использовать только латиницу и дефис."
+        ),
+        validators=[login_validator],
+        error_messages={
+            "unique": "Пользователь с таким именем уже существует.",
+        },
+    )
+    first_name = models.CharField(verbose_name="ФИО", max_length=150, blank=False, validators=[fio_validator],
+                                  help_text=(
+                                      "Разрешается использовать только кириллицу, пробелы и дефис."))
+    email = models.EmailField(verbose_name="Почта", blank=False)
 
 
-class Meta(AbstractUser.Meta):
-    pass
